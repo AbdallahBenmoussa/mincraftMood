93d0460b4ad22693e3259f2eb6ccdce667932971
Bnemoussa Abdallah <benmoussaabdallah0@gmail.com>
2024-07-31 13:21:02 +0200

    initial commit

---
+++ src/main/java/net/abdallah/tutorialmod/TutorialMod.java
@@ -0,0 +1,22 @@
+package net.abdallah.tutorialmod;
+
+import net.abdallah.tutorialmod.block.ModBlocks;
+import net.abdallah.tutorialmod.item.ModItemGroups;
+import net.abdallah.tutorialmod.item.ModItems;
+import net.fabricmc.api.ModInitializer;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class TutorialMod implements ModInitializer {
+	public static final String MOD_ID = "tutorialmod" ;
+    public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+	@Override
+	public void onInitialize() {
+		ModItemGroups.registerItemGroups();
+
+		ModItems.registerModItems();
+		ModBlocks.registerModBlocks();
+	}
+}
---
+++ src/main/java/net/abdallah/tutorialmod/TutorialModClient.java
@@ -0,0 +1,10 @@
+package net.abdallah.tutorialmod;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class TutorialModClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
