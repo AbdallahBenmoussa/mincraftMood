5dc93e48b070c8c93caea8be9132aa1eebe001a4
Bnemoussa Abdallah <benmoussaabdallah0@gmail.com>
2024-07-31 22:10:59 +0200

    remove Platinum Block

--- src/main/java/net/abdallah/tutorialmod/block/ModBlocks.java
+++ src/main/java/net/abdallah/tutorialmod/block/ModBlocks.java
@@ -15,8 +15,6 @@ public class ModBlocks {
 
     public static final Block RUBY_BLOCK = registerBlock("ruby_block", new Block(FabricBlockSettings.copyOf(Blocks.IRON_BLOCK)));
     public static final Block RAW_RUBY_BLOCK = registerBlock("raw_ruby_block", new Block(FabricBlockSettings.copyOf(Blocks.RAW_IRON_BLOCK)));
-    public static final Block PLATINUM_BLOCK = registerBlock("platinum_block", new Block(FabricBlockSettings.copyOf(Blocks.DIAMOND_BLOCK)));
-
 
     private static Block registerBlock(String name ,Block block) {
         registerBlockItem(name , block) ;
